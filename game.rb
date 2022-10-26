467b2dd720ce93c320d5af7098683d437dee68a9
John Ericsson Robarios <ercsssn@gmail.com>
2022-10-26 18:14:29 +0800

    Create the initialize funtion to setup players

--- game.rb
+++ game.rb
@@ -2,6 +2,12 @@ require_relative 'display_text.rb'
 
 class Game
   include Display
+  
+  def initialize
+    @first_player = nil
+    @second_player = nil
+    @current_player = nil
+  end
 
   def game_set_up
     puts welcome_text
